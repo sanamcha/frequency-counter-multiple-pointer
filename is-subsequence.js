52c35dd15203a82ac9aa4c69c14de9bab18d484f
Sanam <64443718+sanamcha@users.noreply.github.com>
2022-05-26 18:45:53 -0700

    Update is-subsequence.js

--- is-subsequence.js
+++ is-subsequence.js
@@ -1,16 +1,16 @@
 // add whatever parameters you deem necessary
 function isSubsequence(str1, str2) {
-    let idx1 = 0;
-    let idx2 = 0;
+    let i = 0;
+    let j = 0;
     if(!str1) return false;
 
-    while(idx2 < str2.length) {
-        if(str2[idx2] === str1[idx1]){
-            idx1 += 1;
+    while(j < str2.length) {
+        if(str2[j] === str1[i]){
+            i += 1;
         }
-        if(idx1 === str1.length)
+        if(i === str1.length)
         return true;
-        idx2 += 1;
+        j += 1;
     }
     return false;
 }
